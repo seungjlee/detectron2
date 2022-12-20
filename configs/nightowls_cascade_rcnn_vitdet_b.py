0d821fb64aab99e7a7320e5ff5a2ad025bae6a36
Seung Jae Lee <seungjaelee@mitre.org>
2022-12-19 20:20:34 -0500

    Make configuring easier.

--- configs/nightowls_cascade_rcnn_vitdet_b.py
+++ configs/nightowls_cascade_rcnn_vitdet_b.py
@@ -8,18 +8,23 @@ from detectron2.modeling.roi_heads import (
     CascadeROIHeads,
 )
 
-from .nightowls_rcnn_vitdet_b import (
-    dataloader,
-    lr_multiplier,
-    model,
-    optimizer,
-    train,
-)
+from .nightowls_rcnn_vitdet_b import GetConfigs
 
+IMAGE_SIZE = 640  # For original model configuration, set this to 1024.
+TRAIN_BATCH_SIZE = 16
 BOX_HEADS_FULLY_CONNECTED_DIM = 1024
-TRAIN_BATCH_SIZE = 16  # 16*10000 = 160000 > 1 epoch
 
-dataloader.train.total_batch_size = TRAIN_BATCH_SIZE
+# These seem to get overriden so they are set again in the yaml file.
+# Need to figure this out.
+MAX_ITERATIONS = 62000  # 62000 ~ 10 epochs
+SCHEDULER_STEPS = [12400, 49600]
+
+dataloader, lr_multiplier, model, optimizer, train = GetConfigs(
+    image_size=IMAGE_SIZE,
+    train_batch_size=TRAIN_BATCH_SIZE,
+    max_iterations=MAX_ITERATIONS,
+    scheduler_steps=SCHEDULER_STEPS,
+)
 
 # arguments that don't exist for Cascade R-CNN
 [model.roi_heads.pop(k) for k in ["box_head", "box_predictor", "proposal_matcher"]]
@@ -51,4 +56,5 @@ model.roi_heads.update(
     ],
 )
 
+# MODEL.WEIGHTS in yaml file used when running train_net.py
 train.init_checkpoint = "cascade_mask_rcnn_vitdet_b_model_final_435fa9.pkl"
--- configs/nightowls_rcnn_vitdet_b.py
+++ configs/nightowls_rcnn_vitdet_b.py
@@ -9,58 +9,53 @@ from detectron2.modeling.backbone.vit import get_vit_lr_decay_rate
 #from ..common.coco_loader_lsj import dataloader
 import detectron2.data.transforms as T
 
-IMAGE_SIZE = 640  # For original model configuration, set this to 1024.
-TRAIN_BATCH_SIZE = 16
+def GetConfigs(image_size=640, train_batch_size=4, max_iterations = 10000, scheduler_steps = (2000, 8000)):
+    #region Data Loader
+    # Data using LSJ
+    dataloader = model_zoo.get_config("nightowls.py").dataloader
+    dataloader.train.mapper.augmentations = [
+        L(T.RandomFlip)(horizontal=True),  # flip first
+        L(T.ResizeScale)(
+            min_scale=0.1, max_scale=2.0, target_height=image_size, target_width=image_size
+        ),
+        L(T.FixedSizeCrop)(crop_size=(image_size, image_size), pad=False),
+    ]
+    dataloader.train.mapper.image_format = "RGB"
+    dataloader.train.total_batch_size = train_batch_size
+    dataloader.train.num_workers = 1
+    dataloader.train.mapper.recompute_boxes = False
 
-# These seem to get overriden so they are set again in nightowls.yaml.
-# Need to figure this out.
-MAX_ITERATIONS = 62000  # 62000 ~ 10 epochs
-SCHEDULER_STEPS = [12400, 49600]
+    dataloader.test.mapper.augmentations = [
+        L(T.ResizeShortestEdge)(short_edge_length=image_size, max_size=image_size),
+    ]
+    #endregion
 
-#region Data Loader
-# Data using LSJ
-dataloader = model_zoo.get_config("nightowls.py").dataloader
-dataloader.train.mapper.augmentations = [
-    L(T.RandomFlip)(horizontal=True),  # flip first
-    L(T.ResizeScale)(
-        min_scale=0.1, max_scale=2.0, target_height=IMAGE_SIZE, target_width=IMAGE_SIZE
-    ),
-    L(T.FixedSizeCrop)(crop_size=(IMAGE_SIZE, IMAGE_SIZE), pad=False),
-]
-dataloader.train.mapper.image_format = "RGB"
-dataloader.train.total_batch_size = TRAIN_BATCH_SIZE
-dataloader.train.num_workers = 1
-dataloader.train.mapper.recompute_boxes = False
+    model = model_zoo.get_config("common/models/mask_rcnn_vitdet.py").model
+    model.backbone.net.img_size = image_size
+    model.backbone.square_pad = image_size
 
-dataloader.test.mapper.augmentations = [
-    L(T.ResizeShortestEdge)(short_edge_length=IMAGE_SIZE, max_size=IMAGE_SIZE),
-]
-#endregion
+    # Initialization and trainer settings
+    train = model_zoo.get_config("common/train.py").train
+    train.amp.enabled = True
+    train.ddp.fp16_compression = True
+    train.init_checkpoint = "mask_rcnn_vitdet_b_model_final_61ccd1.pkl"
 
-model = model_zoo.get_config("common/models/mask_rcnn_vitdet.py").model
-model.backbone.net.img_size = IMAGE_SIZE
-model.backbone.square_pad = IMAGE_SIZE
+    # Schedule
+    train.max_iter = max_iterations
 
-# Initialization and trainer settings
-train = model_zoo.get_config("common/train.py").train
-train.amp.enabled = True
-train.ddp.fp16_compression = True
-train.init_checkpoint = "mask_rcnn_vitdet_b_model_final_61ccd1.pkl"
+    lr_multiplier = L(WarmupParamScheduler)(
+        scheduler=L(MultiStepParamScheduler)(
+            values=[1.0, 0.1, 0.01],
+            milestones=scheduler_steps,
+            num_updates=train.max_iter,
+        ),
+        warmup_length=250 / train.max_iter,
+        warmup_factor=0.001,
+    )
 
-# Schedule
-train.max_iter = MAX_ITERATIONS
+    # Optimizer
+    optimizer = model_zoo.get_config("common/optim.py").AdamW
+    optimizer.params.lr_factor_func = partial(get_vit_lr_decay_rate, num_layers=12, lr_decay_rate=0.7)
+    optimizer.params.overrides = {"pos_embed": {"weight_decay": 0.0}}
 
-lr_multiplier = L(WarmupParamScheduler)(
-    scheduler=L(MultiStepParamScheduler)(
-        values=[1.0, 0.1, 0.01],
-        milestones=SCHEDULER_STEPS,
-        num_updates=train.max_iter,
-    ),
-    warmup_length=250 / train.max_iter,
-    warmup_factor=0.001,
-)
-
-# Optimizer
-optimizer = model_zoo.get_config("common/optim.py").AdamW
-optimizer.params.lr_factor_func = partial(get_vit_lr_decay_rate, num_layers=12, lr_decay_rate=0.7)
-optimizer.params.overrides = {"pos_embed": {"weight_decay": 0.0}}
+    return dataloader, lr_multiplier, model, optimizer, train
